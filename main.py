ccf890ed8d5f912f6ed60a9aaf2cb7479c837f6c
pedrohilan <pedro.hilan99@gmail.com>
2022-08-26 14:44:27 -0300

    primeira_versao

--- main.py
+++ main.py
@@ -2,10 +2,13 @@ from os import getenv
 from dotenv import load_dotenv
 from uvloop import install
 from pyrogram import Client, filters
-#from pyrogram.types import ReplyKeyboardMarkup
+from pyrogram.types import ReplyKeyboardMarkup
 import json
 import gspread
 from oauth2client.service_account import ServiceAccountCredentials
+from datetime import datetime
+
+dateNow = datetime.today().strftime('%d/%m/%Y %H:%M')
 
 load_dotenv()
 install()
@@ -29,19 +32,38 @@ client = gspread.authorize(creds)
 spreadsheet = client.open_by_url("https://docs.google.com/spreadsheets/d/1TmPohMBPI2bsxYsWs7p9mv19FOiUH6NnzRNJbXSc3Q4/edit#gid=16028244")
 sheet = spreadsheet.sheet1
 
-@app.on_message(filters.command('iniciar'))
+@app.on_message(filters.command(['iniciar', 'start']))
 async def start(client, message):
-    await message.reply("Digite o comando '/últimos' para os 5 últimos registros da ficha de encaminhamento Google")
+    await message.reply("Digite o comando '/teclado' para abrir as opções")
+
+@app.on_message(filters.command('teclado'))
+async def keyboard(client, message):
+    teclado=ReplyKeyboardMarkup(
+        [
+            ['/últimos', '/nada', '/nada'],
+            ['/nada', '/nada', 'nada']
+        ], 
+        resize_keyboard=True)
+    await message.reply('Escolha uma opção', reply_markup=teclado)
 
 @app.on_message(filters.command('últimos'))
 async def last(client, message):
     
     # Extract and print all of the values
     rows = sheet.get_all_records()
-    msg = ''
+    msg = 'Esses foram os 5 últimos registros da tabela em '+dateNow+'\n\n'
     for i in range(1,6):
         msg += "**Adolescente/Jovem** : "+rows[-i]['Nome do(a) Adolescente/Jovem']+"\n** Enviado em:** "+rows[-i]['Carimbo de data/hora']+"\n\n"
     
     await message.reply(msg)
+
+@app.on_message(filters.command('nada'))
+async def nothing(client, message):
+    await message.reply('nada por enquanto')
+
+@app.on_message()
+async def anything(client, message):
+    await message.reply("Digite /start ou /iniciar para iniciar")
+
 
 app.run()
